8c5d85ca6a8f0e337d03b5767a1b869db2b5a648
dn74iiit <nindra25074@iiitd.ac.in>
2026-01-24 23:10:14 +0530

    Delete GRS_PA01/MT25074_Part_B_Program.h

--- GRS_PA01/MT25074_Part_B_Program.h
+++
@@ -1,8 +0,0 @@
-#ifndef WORKERS_H //makes sure no where else the functions are included again
-#define WORKERS_H
-
-void cpu(long long input);
-void mem(long long input);
-void io(long long input);
-
-#endif
