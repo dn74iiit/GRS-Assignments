aa8886d0ecfb9f97a13b49bef7627cb61fe59785
dn74iiit <nindra25074@iiitd.ac.in>
2026-01-24 22:33:22 +0530

    Add files via upload

---
+++ GRS_PA01/MT25074_Part_B_Program.h
@@ -0,0 +1,8 @@
+#ifndef WORKERS_H //makes sure no where else the functions are included again
+#define WORKERS_H
+
+void cpu(long long input);
+void mem(long long input);
+void io(long long input);
+
+#endif
